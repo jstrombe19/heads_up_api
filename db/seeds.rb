b128e95d78d49776b1df15afd39b2a7d44670ac6
jstrombe19 <jaredstromberg@gmail.com>
2020-03-26 15:55:01 -0700

    Added four seeds - two each for Ruby and Rails.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 ruby = Category.create({title: "Ruby"})
+rails = Category.create({title: "Ruby on Rails"})
+javascript = Category.create({title: "JavaScript"})
+reactjs = Category.create({title: "Reactjs"})
+general = Category.create({title: "General Knowledge"})
 
-sample = Card.create({
+ruby_sample1 = Card.create({
   question: "What does Array.select() return?", 
-  answer: "Array.select() returns a new array with all of the elements from the original array for which the specified condition evaluates true.", 
+  answer: "Array.select() returns a new array with all of the 
+    elements from the original array for which the specified 
+    condition evaluates true.", 
   difficulty: 1, 
   category_id: ruby
+})
+
+ruby_sample2 = Card.create({
+  question: "What purpose do threads in Ruby serve?", 
+  answer: "Threads enable Ruby to handle multiple threads of execution simultaneously.", 
+  difficulty: 4, 
+  category_id: ruby
+})
+
+rails_sample1 = Card.create({
+  question: "How do you create a Rails API?",
+  answer: "Add --api in the intial rails new command.",
+  difficulty: 1,
+  category_id: 2
+})
+
+rails_sample2 = Card.create({
+  question: "What is source used for when defining relationships?",
+  answer: "Source is used to explicitly define which class a 
+    relationship points to when its name cannot be implicitly 
+    identified by Rails.",
+  difficulty: 4,
+  category_id: 2
 })
